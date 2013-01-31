aa7bd717a11b151fbd2db198db54bf10c4a396a3
RoshanWu <wwj1983@gmail.com>
2013-01-31 13:39:18 +0800

    initial commit

---
+++ js/vangle.js
@@ -0,0 +1,39 @@
+/**
+ * 导航下拉
+ */
+$(function () {
+  var $dropdown = $(".dropdown");
+  var DC = {
+    timer: []
+  };
+
+  $dropdown.each(function (i, oElem) {
+    oElem = this;
+    $(this).mouseover(function () {
+      if (DC.timer[i]) clearTimeout(DC.timer[i]);
+      DC.timer[i] = setTimeout(function () {
+        $(oElem).attr('id', 'expansion');
+        $('.nav-layer').eq(i).show()
+          .css({
+            left: $(oElem).position().left - parseInt($('.nav-layer').eq(i).width()) + 1
+          })
+          .mouseenter(function () {
+            if (DC.timer[i]) clearTimeout(DC.timer[i]);
+          }).
+          mouseleave(function () {
+            var $layer = $(this);
+            DC.timer[i] = setTimeout(function () {
+              $(oElem).attr('id', '');
+              $layer.hide();
+            }, 500);
+          });
+      }, 500);
+    }).mouseout(function () {
+        if (DC.timer[i]) clearTimeout(DC.timer[i]);
+        DC.timer[i] = setTimeout(function () {
+          $(oElem).attr('id', '');
+          $('.nav-layer').eq(i).hide();
+        }, 500);
+      });
+  });
+});
