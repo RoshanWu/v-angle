f756f6dc87f416fef7f49f3869967f0fecab67bd
RoshanWu <wwj1983@gmail.com>
2013-12-16 13:51:28 +0800

    2013.12.16

--- js/vangle.js
+++ js/vangle.js
@@ -50,3 +50,35 @@ $(function () {
     $('#hook').animate({ marginLeft: '0' });
   }
 });
+
+/* 文本域自动扩展
+*/
+
+$.fn.extend({
+    expand: function (minHeight, maxHeight) {
+        return this.each(function () {
+            new $.TextAreaExpander(this, minHeight, maxHeight);
+        });
+    }
+});
+
+$.TextAreaExpander = function (t, minHeight, maxHeight) {
+
+    // 判断是否为文本域 textarea
+    if (t.nodeName.toLowerCase() != "textarea")
+        return;
+
+    var padding = parseInt($(t).css("padding-top")) + parseInt($(t).css("padding-bottom"));
+
+    resize(t);
+
+    $(t).keyup(resize);
+
+    function resize(e) {
+        e = e.target || e;
+        if (!document.all) e.style.height = "0px";
+        var h = Math.max(minHeight || 0, Math.min(e.scrollHeight, maxHeight || 99999)); // 限定高度范围
+        e.style.overflow = (e.scrollHeight > h) ? "auto" : "hidden"; // 超过限定高度，出现滚动条
+        e.style.height = (document.all) ? h - padding + "px" : h - padding + "px";
+    }
+};
